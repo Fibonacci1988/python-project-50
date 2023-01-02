8e38966b644ffcce6bd32011a1823f0865e3c633
Shyngys Zhakelov <zhakelov.shyngys@gmail.com>
2023-01-03 00:25:14 +0500

    Step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,10 @@ import argparse
 
 
 def main():
-    # Create an ArgumentParser object
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    # Add the positional arguments
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',help='set format of output')
     # Parse the command-line arguments
     args = parser.parse_args()
 
